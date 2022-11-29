9445ed75538f443fde50c43c42bbf519a2ba19f4
ruisantiago <ruisantiagomr@gmail.com>
2022-11-29 16:08:16 +0000

    add tests

---
+++ src/tests/nearWallet.test.ts
@@ -0,0 +1,10 @@
+import { NearRPC } from '../../types';
+import { nearWalletExists } from '../index';
+
+test('Does this wallet exist on testnet?', async () => {
+  expect(await nearWalletExists('1.testnet', NearRPC.TESTNET)).toBe(true);
+});
+
+test('Does this wallet exist on mainnet?', async () => {
+  expect(await nearWalletExists('1.testnet', NearRPC.MAINNET)).toBe(false);
+});
